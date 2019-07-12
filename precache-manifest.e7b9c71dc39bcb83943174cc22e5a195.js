32f78ded105208c2035ce563564f6f27632482b4
vadim.gurov <vadim.gurov@gmail.com>
2019-07-12 12:57:48 +0300

    Updates

---
+++ precache-manifest.e7b9c71dc39bcb83943174cc22e5a195.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c232d2e8c4f4a00336865ff96a24f960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397fcc7666537b69be2c",
+    "url": "/static/css/main.dcd8ddc0.chunk.css"
+  },
+  {
+    "revision": "d26b8f169e255ed33801",
+    "url": "/static/js/2.edbf5967.chunk.js"
+  },
+  {
+    "revision": "397fcc7666537b69be2c",
+    "url": "/static/js/main.7f0e166f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5d6bc2ce5822e5a5c843b55ff607a27",
+    "url": "/static/media/arrow.a5d6bc2c.svg"
+  },
+  {
+    "revision": "b5295dad805790ee3cd9139c39b084b4",
+    "url": "/static/media/friend.b5295dad.svg"
+  },
+  {
+    "revision": "926a407d8e47a28ce91561fe4b009dbf",
+    "url": "/static/media/game.926a407d.svg"
+  },
+  {
+    "revision": "0866743ae8957f86d8ded17c36fc2503",
+    "url": "/static/media/group.0866743a.svg"
+  },
+  {
+    "revision": "d65ee88293155c491d457295794adab4",
+    "url": "/static/media/remove.d65ee882.svg"
+  }
+]);
